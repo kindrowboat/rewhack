f9c92216c7c8daa5e90aff57b52f2e408ddfcf4f
Tom Dunlap <tom@cando.com>
2016-12-27 21:00:46 +0000

    Generate holes programatically

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -3,25 +3,39 @@ import './GameBoard.css';
 import MoleHole from './MoleHole';
 
 class GameBoard extends Component {
-  
+
+  rows = 3;
+  holesPerRow = 3;
+
+  constructor(props){
+    super(props);
+    this.state = { holes: [] };
+    for(let i = 0; i < this.rows * this.holesPerRow; i++) {
+      this.state.holes.push({mole: false, whacked: false});
+    }
+  }
+
+  moleRow(rowNum){
+    const startIndex = rowNum * this.holesPerRow;
+    const stopIndex = startIndex + this.holesPerRow;
+    const holes = [];
+
+    for(let i = startIndex; i < stopIndex; i++){
+      const holeState = this.state.holes[i];
+      holes.push(<MoleHole {...holeState}/>);
+    }
+
+    return(
+      <div className="MoleRow">
+        { holes }
+      </div>
+    );
+  }
+
   render(){
     return (
       <div className="GameBoard">
-        <div className="MoleRow">
-          <MoleHole mole/>
-          <MoleHole/>
-          <MoleHole whacked/>
-        </div>
-        <div className="MoleRow">
-          <MoleHole/>
-          <MoleHole/>
-          <MoleHole/>
-        </div>
-        <div className="MoleRow">
-          <MoleHole/>
-          <MoleHole mole whacked/>
-          <MoleHole/>
-        </div>
+        { Array(this.rows).fill().map((_, i) => this.moleRow(i)) }
       </div>
     );
   }
